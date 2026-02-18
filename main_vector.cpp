051046613dfce748057ae9fe74143acc8602adbb
LastTaterTot <joringis.p@gmail.com>
2026-02-18 16:38:47 +0200

    Pridėtas meniu

--- main_vector.cpp
+++ main_vector.cpp
@@ -14,20 +14,27 @@ struct Student {
     int egz = 0;
 };
 
+static random_device rd;
+static mt19937 gen(rd());
+
+int randInt(int a, int b) {
+    uniform_int_distribution<int> dist(a, b);
+    return dist(gen);
+}
+
 double vidurkis(const vector<int>& nd) {
-    if (nd.empty()) return 0;
-    double s = 0;
+    if (nd.empty()) return 0.0;
+    double s = 0.0;
     for (int x : nd) s += x;
     return s / nd.size();
 }
 
 double mediana(vector<int> nd) {
-    if (nd.empty()) return 0;
+    if (nd.empty()) return 0.0;
     sort(nd.begin(), nd.end());
-    int n = nd.size();
-    if (n % 2 == 0)
-        return (nd[n / 2 - 1] + nd[n / 2]) / 2.0;
-    return nd[n / 2];
+    int n = (int)nd.size();
+    if (n % 2 == 0) return (nd[n/2 - 1] + nd[n/2]) / 2.0;
+    return nd[n/2];
 }
 
 double galutinis(const Student& s, bool naudotiMediana) {
@@ -35,90 +42,135 @@ double galutinis(const Student& s, bool naudotiMediana) {
     return 0.4 * ndRez + 0.6 * s.egz;
 }
 
-int randomPazymys() {
-    static random_device rd;
-    static mt19937 gen(rd());
-    uniform_int_distribution<> dist(1, 10);
-    return dist(gen);
+void printTable(const vector<Student>& st, bool naudotiMediana) {
+    cout << "\n";
+    cout << left << setw(16) << "Pavarde"
+         << left << setw(16) << "Vardas"
+         << (naudotiMediana ? "Galutinis (Med.)" : "Galutinis (Vid.)") << "\n";
+    cout << string(16 + 16 + 18, '-') << "\n";
+
+    cout << fixed << setprecision(2);
+    for (const auto& s : st) {
+        cout << left << setw(16) << s.pavarde
+             << left << setw(16) << s.vardas
+             << galutinis(s, naudotiMediana) << "\n";
+    }
 }
 
-vector<Student> rankinisIvedimas() {
-    vector<Student> studentai;
-
+vector<int> inputND_until0() {
+    vector<int> nd;
+    cout << "Veskite ND (1-10), 0 - baigti ND:\n";
     while (true) {
-        Student s;
-        cout << "Vardas (0 baigti): ";
-        cin >> s.vardas;
-        if (s.vardas == "0") break;
-
-        cout << "Pavarde: ";
-        cin >> s.pavarde;
-
-        cout << "ND (0 baigti): ";
-        while (true) {
-            int x;
-            cin >> x;
-            if (x == 0) break;
-            s.nd.push_back(x);
-        }
-
-        cout << "Egzaminas: ";
-        cin >> s.egz;
-
-        studentai.push_back(s);
+        cout << "ND: ";
+        int x; cin >> x;
+        if (x == 0) break;
+        if (x >= 1 && x <= 10) nd.push_back(x);
+        else cout << "Klaida: pazymys 1-10.\n";
     }
-
-    return studentai;
+    return nd;
 }
 
-vector<Student> generavimas() {
-    int m, n;
-    cout << "Studentu skaicius: ";
-    cin >> m;
-    cout << "ND kiekis: ";
-    cin >> n;
+Student inputStudent_full() {
+    Student s;
+    cout << "Vardas: ";
+    cin >> s.vardas;
+    cout << "Pavarde: ";
+    cin >> s.pavarde;
+    s.nd = inputND_until0();
+    cout << "Egzaminas (1-10): ";
+    cin >> s.egz;
+    return s;
+}
 
-    vector<Student> studentai;
+Student inputStudent_nameOnly_generateGrades(int ndKiek) {
+    Student s;
+    cout << "Vardas: ";
+    cin >> s.vardas;
+    cout << "Pavarde: ";
+    cin >> s.pavarde;
 
-    for (int i = 0; i < m; i++) {
-        Student s;
-        s.vardas = "Vardas" + to_string(i + 1);
-        s.pavarde = "Pavarde" + to_string(i + 1);
+    s.nd.clear();
+    s.nd.reserve(ndKiek);
+    for (int i = 0; i < ndKiek; i++) s.nd.push_back(randInt(1, 10));
+    s.egz = randInt(1, 10);
+    return s;
+}
 
-        for (int j = 0; j < n; j++)
-            s.nd.push_back(randomPazymys());
-
-        s.egz = randomPazymys();
-        studentai.push_back(s);
-    }
-
-    return studentai;
+Student generateStudent_all(int idx, int ndKiek) {
+    Student s;
+    s.vardas  = "Vardas" + to_string(idx);
+    s.pavarde = "Pavarde" + to_string(idx);
+    s.nd.reserve(ndKiek);
+    for (int i = 0; i < ndKiek; i++) s.nd.push_back(randInt(1, 10));
+    s.egz = randInt(1, 10);
+    return s;
 }
 
 int main() {
-    cout << "1 - Vidurkis\n2 - Mediana\n";
-    int pasirinkimas;
-    cin >> pasirinkimas;
-    bool naudotiMediana = pasirinkimas == 2;
+    cout << "Skaiciavimo budas:\n1 - Vidurkis\n2 - Mediana\nPasirinkimas: ";
+    int mode; cin >> mode;
+    bool naudotiMediana = (mode == 2);
 
-    cout << "1 - Rankinis ivedimas\n2 - Generavimas\n";
-    int tipas;
-    cin >> tipas;
+    vector<Student> studentai;
 
-    vector<Student> studentai =
-        (tipas == 1) ? rankinisIvedimas() : generavimas();
+    while (true) {
+        cout << "\nMeniu:\n";
+        cout << "1 - Ivedimas ranka (vardas, pavarde, ND, egz)\n";
+        cout << "2 - Generuoti tik pazymius (ivedi varda+pavarde)\n";
+        cout << "3 - Generuoti varda+pavarde+pazymius\n";
+        cout << "4 - Baigti\n";
+        cout << "Pasirinkimas: ";
 
-    cout << fixed << setprecision(2);
+        int pasirinkimas;
+        cin >> pasirinkimas;
 
-    cout << left << setw(15) << "Pavarde"
-         << setw(15) << "Vardas"
-         << "Galutinis\n";
+        if (pasirinkimas == 4) break;
 
-    cout << "--------------------------------\n";
+        if (pasirinkimas == 1) {
+            cout << "\nIveskite studenta. (Vardas 0 - grizti i meniu)\n";
+            while (true) {
+                cout << "Vardas (0 - stop): ";
+                string v; cin >> v;
+                if (v == "0") break;
 
-    for (auto& s : studentai) {
-        cout << setw(15) << s.pavarde
-             << setw(15) << s.vardas
-             << galutinis(s, naudotiMediana) << "\n";
+                Student s;
+                s.vardas = v;
+                cout << "Pavarde: ";
+                cin >> s.pavarde;
+                s.nd = inputND_until0();
+                cout << "Egzaminas (1-10): ";
+                cin >> s.egz;
+
+                studentai.push_back(s);
+            }
+            printTable(studentai, naudotiMediana);
+        }
+        else if (pasirinkimas == 2) {
+            int m, n;
+            cout << "Kiek studentu ivesite? "; cin >> m;
+            cout << "Kiek ND generuoti kiekvienam? "; cin >> n;
+
+            for (int i = 0; i < m; i++) {
+                cout << "\nStudentas #" << (i + 1) << "\n";
+                studentai.push_back(inputStudent_nameOnly_generateGrades(n));
+            }
+            printTable(studentai, naudotiMediana);
+        }
+        else if (pasirinkimas == 3) {
+            int m, n;
+            cout << "Kiek studentu generuoti? "; cin >> m;
+            cout << "Kiek ND generuoti kiekvienam? "; cin >> n;
+
+            for (int i = 1; i <= m; i++) {
+                studentai.push_back(generateStudent_all(i, n));
+            }
+            printTable(studentai, naudotiMediana);
+        }
+        else {
+            cout << "Neteisingas pasirinkimas.\n";
+        }
     }
+
+    cout << "Baigta.\n";
+    return 0;
 }
