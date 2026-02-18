cf58e57f5e530526a91e663f1ee855501eebace1
LastTaterTot <joringis.p@gmail.com>
2026-02-18 16:29:30 +0200

    prideta mediana ir pasirinkimas

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iomanip>
 #include <iostream>
 #include <string>
@@ -9,56 +10,71 @@ struct Student {
     string vardas;
     string pavarde;
     vector<int> nd;
-    int egz;
+    int egz = 0;
 };
 
 double vidurkis(const vector<int>& nd) {
-    if (nd.empty()) return 0;
-    double s = 0;
-    for (int x : nd) s += x;
-    return s / nd.size();
+    if (nd.empty()) return 0.0;
+    double sum = 0;
+    for (int x : nd) sum += x;
+    return sum / nd.size();
 }
 
-double galutinis(const Student& s) {
-    return 0.4 * vidurkis(s.nd) + 0.6 * s.egz;
+double mediana(vector<int> nd) {
+    if (nd.empty()) return 0.0;
+    sort(nd.begin(), nd.end());
+    size_t n = nd.size();
+    if (n % 2 == 1) return nd[n / 2];
+    return (nd[n / 2 - 1] + nd[n / 2]) / 2.0;
+}
+
+double galutinis(const Student& s, bool naudotiMediana) {
+    double ndRez = naudotiMediana ? mediana(s.nd) : vidurkis(s.nd);
+    return 0.4 * ndRez + 0.6 * s.egz;
 }
 
 int main() {
     vector<Student> studentai;
 
+    cout << "Skaiciavimo budas:\n1 - Vidurkis\n2 - Mediana\nPasirinkimas: ";
+    int mode;
+    cin >> mode;
+    bool naudotiMediana = (mode == 2);
+
     while (true) {
         Student s;
-        cout << "Vardas (0 baigti): ";
+        cout << "\nVardas (0 - baigti): ";
         cin >> s.vardas;
         if (s.vardas == "0") break;
 
         cout << "Pavarde: ";
         cin >> s.pavarde;
 
-        cout << "ND (0 baigti): ";
+        cout << "ND pazymiai (1-10), 0 - baigti ND:\n";
         while (true) {
+            cout << "ND: ";
             int x;
             cin >> x;
             if (x == 0) break;
             s.nd.push_back(x);
         }
 
-        cout << "Egzaminas: ";
+        cout << "Egzaminas (1-10): ";
         cin >> s.egz;
 
         studentai.push_back(s);
     }
 
-    cout << left << setw(15) << "Pavarde"
-         << setw(15) << "Vardas"
-         << "Galutinis (Vid.)\n";
-
-    cout << "-------------------------------------\n";
+    cout << "\n";
+    cout << left << setw(16) << "Pavarde"
+         << left << setw(16) << "Vardas"
+         << (naudotiMediana ? "Galutinis (Med.)" : "Galutinis (Vid.)") << "\n";
+    cout << string(16 + 16 + 18, '-') << "\n";
 
     cout << fixed << setprecision(2);
-    for (auto& s : studentai) {
-        cout << setw(15) << s.pavarde
-             << setw(15) << s.vardas
-             << galutinis(s) << "\n";
+    for (const auto& s : studentai) {
+        cout << left << setw(16) << s.pavarde
+             << left << setw(16) << s.vardas
+             << galutinis(s, naudotiMediana) << "\n";
     }
 }
