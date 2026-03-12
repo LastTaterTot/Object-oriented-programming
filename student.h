e18337f35c03c2a824a36709329612f5be420b9e
LastTaterTot <joringis.p@gmail.com>
2026-03-12 15:05:37 +0200

    Create student.h

---
+++ student.h
@@ -0,0 +1,14 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <string>
+#include <vector>
+
+struct Student {
+    std::string vardas;
+    std::string pavarde;
+    std::vector<int> nd;
+    int egz;
+};
+
+#endif
