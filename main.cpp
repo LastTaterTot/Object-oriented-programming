60f62ff15931304b220c113c64f1f164bf473931
LastTaterTot <joringis.p@gmail.com>
2026-05-04 21:36:39 +0300

    perdarymas versijai 1.0

--- main.cpp
+++ main.cpp
@@ -1,80 +1,106 @@
-#include <algorithm>
-#include <iomanip>
+#include "functions.h"
 #include <iostream>
-#include <string>
 #include <vector>
+#include <list>
+#include <deque>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
-struct Student {
-    string vardas;
-    string pavarde;
-    vector<int> nd;
-    int egz = 0;
-};
+// ================= TEST FUNKCIJA =================
+template <typename Container>
+void runTest(const string& filename, const string& typeName) {
 
-double vidurkis(const vector<int>& nd) {
-    if (nd.empty()) return 0.0;
-    double sum = 0;
-    for (int x : nd) sum += x;
-    return sum / nd.size();
+    cout << "\n==== " << typeName << " ====\n";
+
+    // ---------- READ ----------
+    auto start = high_resolution_clock::now();
+    Container students = readFile<Container>(filename);
+    auto readEnd = high_resolution_clock::now();
+
+    cout << "Nuskaitymas: "
+         << duration<double>(readEnd - start).count() << " s\n";
+
+    // ---------- STRATEGY 1 ----------
+    {
+        Container vargsiukai, kietiakai;
+
+        auto t1 = high_resolution_clock::now();
+        splitStrategy1(students, vargsiukai, kietiakai);
+        auto t2 = high_resolution_clock::now();
+
+        cout << "Strategija 1: "
+             << duration<double>(t2 - t1).count() << " s\n";
+    }
+
+    // ---------- STRATEGY 2 ----------
+    {
+        Container temp = students;
+        Container vargsiukai;
+
+        auto t1 = high_resolution_clock::now();
+        splitStrategy2(temp, vargsiukai);
+        auto t2 = high_resolution_clock::now();
+
+        cout << "Strategija 2: "
+             << duration<double>(t2 - t1).count() << " s\n";
+    }
+
+    // ---------- STRATEGY 3 ----------
+    {
+        Container temp = students;
+        Container vargsiukai, kietiakai;
+
+        auto t1 = high_resolution_clock::now();
+        splitStrategy3(temp, vargsiukai, kietiakai);
+        auto t2 = high_resolution_clock::now();
+
+        cout << "Strategija 3 (partition): "
+             << duration<double>(t2 - t1).count() << " s\n";
+    }
 }
 
-double mediana(vector<int> nd) {
-    if (nd.empty()) return 0.0;
-    sort(nd.begin(), nd.end());
-    size_t n = nd.size();
-    if (n % 2 == 1) return nd[n / 2];
-    return (nd[n / 2 - 1] + nd[n / 2]) / 2.0;
-}
-
-double galutinis(const Student& s, bool naudotiMediana) {
-    double ndRez = naudotiMediana ? mediana(s.nd) : vidurkis(s.nd);
-    return 0.4 * ndRez + 0.6 * s.egz;
-}
-
+// ================= MAIN =================
 int main() {
-    vector<Student> studentai;
+    try {
+        cout << "1 - Generuoti failus\n";
+        cout << "2 - Testuoti\n";
 
-    cout << "Skaiciavimo budas:\n1 - Vidurkis\n2 - Mediana\nPasirinkimas: ";
-    int mode;
-    cin >> mode;
-    bool naudotiMediana = (mode == 2);
+        int mode;
+        cin >> mode;
 
-    while (true) {
-        Student s;
-        cout << "\nVardas (0 - baigti): ";
-        cin >> s.vardas;
-        if (s.vardas == "0") break;
+        if (mode == 1) {
+            generateFile("studentai1000.txt", 1000);
+            generateFile("studentai10000.txt", 10000);
+            generateFile("studentai100000.txt", 100000);
+            generateFile("studentai1000000.txt", 1000000);
+            generateFile("studentai10000000.txt", 10000000);
 
-        cout << "Pavarde: ";
-        cin >> s.pavarde;
-
-        cout << "ND pazymiai (1-10), 0 - baigti ND:\n";
-        while (true) {
-            cout << "ND: ";
-            int x;
-            cin >> x;
-            if (x == 0) break;
-            s.nd.push_back(x);
+            cout << "Failai sugeneruoti\n";
+            return 0;
         }
 
-        cout << "Egzaminas (1-10): ";
-        cin >> s.egz;
+        cout << "Pasirink faila:\n";
+        cout << "1 - 1k\n2 - 10k\n3 - 100k\n4 - 1M\n";
 
-        studentai.push_back(s);
+        int c;
+        cin >> c;
+
+        string file;
+        if (c==1) file="studentai1000.txt";
+        else if (c==2) file="studentai10000.txt";
+        else if (c==3) file="studentai100000.txt";
+        else if (c==4) file="studentai1000000.txt";
+        else throw runtime_error("Blogas pasirinkimas");
+
+        runTest<vector<Student>>(file, "vector");
+        runTest<list<Student>>(file, "list");
+        runTest<deque<Student>>(file, "deque");
+
+    } catch (exception& e) {
+        cout << e.what() << endl;
     }
 
-    cout << "\n";
-    cout << left << setw(16) << "Pavarde"
-         << left << setw(16) << "Vardas"
-         << (naudotiMediana ? "Galutinis (Med.)" : "Galutinis (Vid.)") << "\n";
-    cout << string(16 + 16 + 18, '-') << "\n";
-
-    cout << fixed << setprecision(2);
-    for (const auto& s : studentai) {
-        cout << left << setw(16) << s.pavarde
-             << left << setw(16) << s.vardas
-             << galutinis(s, naudotiMediana) << "\n";
-    }
+    return 0;
 }
