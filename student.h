0b54f489168eb22c09d6c12e89f025f1d46f312c
LastTaterTot <joringis.p@gmail.com>
2026-05-04 21:34:45 +0300

    Perdarymas versijai 1.0

--- student.h
+++ student.h
@@ -2,13 +2,11 @@
 #define STUDENT_H
 
 #include <string>
-#include <vector>
 
 struct Student {
-    std::string vardas;
-    std::string pavarde;
-    std::vector<int> nd;
-    int egz;
+    std::string name;
+    std::string surname;
+    double finalGrade;
 };
 
 #endif
